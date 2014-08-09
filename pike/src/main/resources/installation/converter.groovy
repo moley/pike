4e971fdc22f3ce49f545e56f06d0247a1412eb6c
Markus Oley <markus.oley@vsa.de>
2014-08-09 22:39:15 +0200

    fixed vagrant extension

--- pike/src/main/resources/installation/converter.groovy
+++
@@ -1,7 +0,0 @@
-File file = new File ("installPike.bat.orig")
-File outputfile = new File ("installPike.bat")
-outputfile.text = file.text.replaceAll('\n', '\n\r')
-
-File file2 = new File ("configureHost.bat.orig")
-File outputfile2 = new File ("configureHost.bat")
-outputfile2.text = file2.text.replaceAll('\n', '\n\r')
