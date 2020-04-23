52923f2dece16b7a0694b96a50eb26086747980f
OleyMa <markus.oley@vsa.de>
2020-04-23 23:00:21 +0200

    eclipse mirror tasks

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,8 +1,8 @@
 mkdir -p gradle/wrapper
 
-wget --directory-prefix=gradle/wrapper https://github.com/moley/pike/blob/master/gradle/wrapper/gradle-wrapper.jar
-wget --directory-prefix=gradle/wrapper https://github.com/moley/pike/blob/master/gradle/wrapper/gradle-wrapper.properties
-wget https://github.com/moley/pike/blob/master/gradlew
+wget --quiet --directory-prefix=gradle/wrapper https://github.com/moley/pike/blob/master/gradle/wrapper/gradle-wrapper.jar
+wget --quiet --directory-prefix=gradle/wrapper https://github.com/moley/pike/blob/master/gradle/wrapper/gradle-wrapper.properties
+wget --quiet https://github.com/moley/pike/blob/master/gradlew
 chmod 777 gradlew
 echo """ buildscript {
    repository {
@@ -13,6 +13,8 @@ echo """ buildscript {
    }
 }
 """ > build.gradle
+
+clear
 
 echo "Pike is installed in path `pwd`."
 echo "Take a look at https://github.com/moley/pike/blob/master/README.md or start your first build with ./gradlew tasks to analyze the available tasks"
